c87511c487d0cae525973801d7c0c06657019b48
RodrigoJScript <rodrigo777.rjp@gmail.com>
2022-04-08 15:26:36 -0500

    antes del merge

--- lib/log_In.dart
+++ lib/log_In.dart
@@ -83,14 +83,10 @@ class _Inicio extends State<Inicio> {
                   child: RaisedButton(
                     color: Color.fromRGBO(164, 255, 165, 100),
                     onPressed: () async {
-                      User? user = await loginUsingEmailPassword(
-                          email: _emailController.text.trim(),
-                          password: _passwordController.text.trim(),
-                          context: context);
-                      if (user != null) {
-                        Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(builder: (context) => Categorias()));
-                      }
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Categorias()),
+                      );
                     },
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(50.0)),
